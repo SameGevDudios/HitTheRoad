8e07da780cda42891fc8d534d3d4bf9862511d15
SameGevDudios <magomed.chapaev1@gmail.com>
2025-02-12 22:22:52 +0300

    Add layer mask for pickup search

--- HitTheRoad/Assets/Scripts/Highlighting/HighlightController/RangeHighlightController.cs
+++ HitTheRoad/Assets/Scripts/Highlighting/HighlightController/RangeHighlightController.cs
@@ -5,17 +5,19 @@ public class RangeHighlightController : IHighlightController
     private Transform _searchTransform;
     private float _searchRadius;
     private bool _pickupFound;
+    private LayerMask _searchMask;
 
-    public RangeHighlightController(Transform searchTransform, float searchRadius)
+    public RangeHighlightController(Transform searchTransform, float searchRadius, LayerMask searchMask)
     {
         _searchTransform = searchTransform;
         _searchRadius = searchRadius;
+        _searchMask = searchMask;
     }
     public void SearchForPickups()
     {
         float radiusOffset = 0.1f;
-        Collider[] colliderOutside = Physics.OverlapSphere(_searchTransform.position, _searchRadius + radiusOffset);
-        Collider[] colliderInside = Physics.OverlapSphere(_searchTransform.position, _searchRadius);
+        Collider[] colliderOutside = Physics.OverlapSphere(_searchTransform.position, _searchRadius + radiusOffset, _searchMask);
+        Collider[] colliderInside = Physics.OverlapSphere(_searchTransform.position, _searchRadius, _searchMask);
         _pickupFound = colliderInside.Length > 0;
         Highlight(colliderOutside, false); // Lowlight pickups outside (but not too far) of search radius
         Highlight(colliderInside, true); // hightlights pickups inside search radius
