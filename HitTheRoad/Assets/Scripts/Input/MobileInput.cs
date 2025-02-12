d0cd4b2fa7213e2d837dd0215c40c9a00e318b23
SameGevDudios <magomed.chapaev1@gmail.com>
2025-02-13 01:01:01 +0300

    Add mobile input

---
+++ HitTheRoad/Assets/Scripts/Input/MobileInput.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class MobileInput : IInput
+{
+    private UIVirtualJoystick _joystick;
+    private Vector2 _previousTouchPosition;
+
+    public MobileInput(UIVirtualJoystick joystick)
+    {
+        _joystick = joystick;
+    }
+    public Vector3 Movement() =>
+         _joystick.CurrentInput;
+    public Vector2 Look()
+    {
+        if (Input.touchCount < 1)
+            return Vector2.zero;
+        Vector2 delta = _previousTouchPosition - Input.GetTouch(0).position;
+        _previousTouchPosition = Input.GetTouch(0).position;
+        return delta;
+    }
+    public Vector3 GrabPoint()
+    {
+        if (Input.touchCount > 0)
+        {
+            return Input.GetTouch(0).position;
+        }
+        return Vector3.zero;
+    }
+    public bool Grab() =>
+        Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
+}
