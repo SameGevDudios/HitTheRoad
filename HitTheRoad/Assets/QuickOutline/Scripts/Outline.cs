858b793a204604055c41b1b00fb86fa7fdc55e95
SameGevDudios <magomed.chapaev1@gmail.com>
2025-02-12 20:55:42 +0300

    Add external color update for outline

--- HitTheRoad/Assets/QuickOutline/Scripts/Outline.cs
+++ HitTheRoad/Assets/QuickOutline/Scripts/Outline.cs
@@ -271,8 +271,8 @@ public class Outline : MonoBehaviour {
 
   void UpdateMaterialProperties() {
 
-    // Apply properties according to mode
-    outlineFillMaterial.SetColor("_OutlineColor", outlineColor);
+        // Apply properties according to mode
+        UpdateColor();
 
     switch (outlineMode) {
       case Mode.OutlineAll:
@@ -306,4 +306,12 @@ public class Outline : MonoBehaviour {
         break;
     }
   }
+    void UpdateColor()
+    {
+        outlineFillMaterial.SetColor("_OutlineColor", outlineColor);
+    }
+    public void UpdateColor(Color color)
+    {
+        outlineFillMaterial.SetColor("_OutlineColor", color);
+    }
 }
