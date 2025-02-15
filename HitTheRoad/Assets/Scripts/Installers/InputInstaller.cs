879552f71795fbe8f04c57b48adfe677b6edaec8
SameGevDudios <magomed.chapaev1@gmail.com>
2025-02-15 16:46:37 +0300

    Add input installer

---
+++ HitTheRoad/Assets/Scripts/Installers/InputInstaller.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using Zenject;
+
+public class InputInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        switch (Application.platform)
+        {
+            case RuntimePlatform.WindowsEditor: 
+                Container.Bind<IInput>().To<MobileInput>().AsSingle().NonLazy();
+                break;
+            case RuntimePlatform.Android:
+                Container.Bind<IInput>().To<MobileInput>().AsSingle().NonLazy();
+                break;
+            default:
+                Debug.LogError("Unsupported platform. Cannot inject dependencies");
+                break;
+        }
+    }
+}
